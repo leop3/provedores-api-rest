b0f3247f6e87d87d0e2fc6ff8402c3bdc3fc17f8
lepo <larancibia@conexia.com>
2020-08-02 22:26:40 -0300

    Se crea el response generico y el proveedoresResponse

---
+++ src/main/java/lepo/provedoresapirest/reponse/Response.java
@@ -0,0 +1,15 @@
+package lepo.provedoresapirest.reponse;
+
+public class Response {
+
+	String mensaje;
+
+	public String getMensaje() {
+		return mensaje;
+	}
+
+	public void setMensaje(String mensaje) {
+		this.mensaje = mensaje;
+	}
+
+}
